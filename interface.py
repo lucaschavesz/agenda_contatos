731c5f51542728f80f036661422343eb67ef5969
lucaschavesz <lucasdemelo426@gmail.com>
2026-07-10 14:30:09 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,6 +5,16 @@ import sqlite3
 conexao = sqlite3.connect('dados.db')
 cursor = conexao.cursor()
 
+#cria tabela contatos
+cursor.execute("""CREATE TABLE IF NOT EXISTS contatos(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT,
+telefone TEXT,
+email TEXT
+)""")
+
+conexao.commit()
+
 janela = tk.Tk()
 
 janela.title('Agenda de Contatos')
